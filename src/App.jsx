482224cdf3039e3a5074c70d02a267bf2e4c1933
Manahil Abid <sammiiiullah@gmail.com>
2025-08-30 21:58:57 +0500

    fully pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Survey from "./Pages/SurveyPage/Survey.tsx";
+import Home from "./Pages/HomePage/Home.jsx";
 
 
 function App() {
   return(
     <div>
-      <Survey/>
+      <Routes>
+        <Route path="/Survey" element={<Survey/>}/>
+        <Route path="/" element={<Home/>}/>
+
+      </Routes>
+
   </div>
   )
 }
